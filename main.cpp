ee6f3731848ef3f7104df7ad0c88d2bc3a132179
endlesscheng <loli.con@qq.com>
2014-06-15 20:13:06 +0800

    set and insert

--- main.cpp
+++ main.cpp
@@ -1,57 +1,49 @@
-/*
-* this code is made by endless
-* Problem: 1107
-* Verdict: Accepted
-* Submission Date: 2014-06-14 22:44:01
-* Time: 0 MS
-* Memory: 1688 KB
-*/
 #include<bits/stdc++.h>
 using namespace std;
 
-#define Inter(v, a, n, b, m) v.resize(set_intersection(a, a + (n), b, b + (m), v.begin()) - v.begin())
-#define SInter(v, a, n, b, m) v.resize((n) + (m)); sort(a, a + (n)); sort(b, b + (m)); Inter(v, a, n, b, m)
-#define Union(v, a, n, b, m) v.resize(set_union(a, a + (n), b, b + (m), v.begin()) - v.begin());
-#define SUnion(v, a, n, b, m) v.resize((n) + (m)); sort(a, a + (n)); sort(b, b + (m)); Union(v, a, n, b, m)
-#define Diff(v, a, n, b, m) v.resize(set_difference(a, a + (n), b, b + (m), v.begin()) - v.begin())
-#define SDiff(v, a, n, b, m) v.resize((n) + (m)); sort(a, a + (n)); sort(b, b + (m)); Diff(v, a, n, b, m)
-#define Sym(v, a, n, b, m) v.resize(set_symmetric_difference(a, a + (n), b, b + (m) v.begin()) - v.begin())
-#define SSym(v, a, n, b, m) v.resize((n) + (m)); sort(a, a + (n)); sort(b, b + (m)); Sym(v, a, n, b, m)
+#define Fin(f) freopen(f, "r", stdin)
+#define Fout(f) freopen(f, "w", stdout)
+#define SR() srand((unsigned)time(NULL))
+#define random(m) ((rand() << 16 | rand()) % m) /// [0,m)之间的伪随机数
 
-typedef long long ll;
+#define all(s) s.begin(), s.end()
+
+#define Inter(v, s, n, b, m) v.resize(set_intersection(s, s + (n), b, b + (m), v.begin()) - v.begin())
+#define SInter(v, s, n, b, m) v.resize((n) + (m)); sort(s, s + (n)); sort(b, b + (m)); Inter(v, s, n, b, m)
+#define Union(v, s, n, b, m) v.resize(set_union(s, s + (n), b, b + (m), v.begin()) - v.begin());
+#define SUnion(v, s, n, b, m) v.resize((n) + (m)); sort(s, s + (n)); sort(b, b + (m)); Union(v, s, n, b, m)
+#define Diff(v, s, n, b, m) v.resize(set_difference(s, s + (n), b, b + (m), v.begin()) - v.begin())
+#define SDiff(v, s, n, b, m) v.resize((n) + (m)); sort(s, s + (n)); sort(b, b + (m)); Diff(v, s, n, b, m)
+#define Sym(v, s, n, b, m) v.resize(set_symmetric_difference(s, s + (n), b, b + (m) v.begin()) - v.begin())
+#define SSym(v, s, n, b, m) v.resize((n) + (m)); sort(s, s + (n)); sort(b, b + (m)); Sym(v, s, n, b, m)
+
 #define PB push_back
 #define MP make_pair
-#define all(a) a.begin(), a.end()
-#define Uni(a) a.resize(unique(all(a)) - a.begin()) /// STL专用
-#define SUni(a) sort(all(a)); Uni(a) /// STL专用
-#define Unii(a, n) (unique(a, a + (n)) - a) /// 使用前请sort
-#define Acc(a, n) (accumulate(a, a + (n), 0)) /// 注意0LL!!!!!以及0.0!!!
-#define AaddB(a, n, b) transform(a, a + (n), b, a, plus<int>()) /// *慢的话就改为For(i, n) a[i] += b[i](注意加int i)
 #define Cnt1(n) (__builtin_popcount(n))
 #define Cntt1(n) (__builtin_popcountll(n)) /// 参数必须为ull类型
 #define cb(n) (32 - __builtin_clz(n))
 #define cbb(n) (64 - __builtin_clzll(n)) /// 参数必须为ull类型
 #define sq(x) ((x) * (x))
 #define Sqrt(n) (int)sqrt(0.5 + n)
-#define mem(a, num) memset(a, num, sizeof(a))
+
+#define PB push_back
+#define MP make_pair
+#define all(s) s.begin(), s.end()
+#define Uni(s) s.resize(unique(all(s)) - s.begin()) /// STL专用
+#define SUni(s) sort(all(s)); Uni(s) /// STL专用
+#define Unii(s, n) (unique(s, s + (n)) - s) /// 使用前请sort
+#define Acc(s, n) (accumulate(s, s + (n), 0)) /// 注意0LL!!!!!以及0.0!!!
+#define AaddB(s, n, b) transform(s, s + (n), b, s, plus<int>()) /// *慢的话就改为For(i, n) s[i] += b[i](注意加int i)
+#define Cnt1(n) (__builtin_popcount(n))
+#define Cntt1(n) (__builtin_popcountll(n)) /// 参数必须为ull类型
+#define cb(n) (32 - __builtin_clz(n))
+#define cbb(n) (64 - __builtin_clzll(n)) /// 参数必须为ull类型
+#define sq(x) ((x) * (x))
+#define Sqrt(n) (int)sqrt(0.5 + n)
+#define mem(s, num) memset(s, num, sizeof(s))
 #define cpy(to, from) memcpy(to, from, sizeof(from))
 #define Rcpy(l, r, b) reverse_copy(l, r, b) /// 注意为左闭右开区间
 
-typedef unsigned long long ull;
-#define gr() greater<int>()
-#define nth(a, k, n) nth_element(a + 0, a + k, a + n) /// *可能要事先--k
-#define nthg(a, k, n) nth_element(a + 0, a + k, a + n, greater<int>()) /// *可能要事先--k
-#define Min(a, n) (*min_element(a, a + (n)))
-#define Max(a, n) (*max_element(a, a + (n)))
-#define Minpos(a, n) (min_element(a, a + (n)) - (a))
-#define Maxpos(a, n) (max_element(a, a + (n)) - (a))
-#define Lowpos(a, x) (lower_bound(all(a), x) - (a.begin()))
-#define Upppos(a, x) (upper_bound(all(a), x) - (a.begin()))
-#define BS(a, n, x) binary_search(a, a + (n), x) /// 返回bool值
-#define Range(a, n, x) equal_range(a, a + (n), x) /// 返回pair
-#define Fpos(a, x) (find(all(a), x) - (a.begin()))
-
-typedef vector<int>::iterator viter;
 #define For(i, n) for (i = 0; i < (n); ++i)
 #define Forr(i, start, n) for (i = start; i < (n); ++i)
 #define Forrr(i, start, n, step) for (i = start; i < (n); i += (step))
@@ -60,65 +52,79 @@ typedef vector<int>::iterator viter;
 #define rForrr(i, n, end, step) for (i = n; i >= (end); i -= (step))
 #define loop(a) for (it = a.begin(); it != a.end(); ++it)
 
-typedef set<int>::iterator siter;
-typedef map<int, int>::iterator miter;
-#define SI(a) scanf("%d", &a)
-#define SII(a, b) scanf("%d%d", &a, &b)
-#define SIII(a, b, c) scanf("%d%d%d", &a, &b, &c)
-#define SIIII(a, b, c, d) scanf("%d%d%d%d", &a, &b, &c, &d)
-#define SIIIII(a, b, c, d, e) scanf("%d%d%d%d%d", &a, &b, &c, &d, &e)
-#define SIIIIII(a, b, c, d, e, f) scanf("%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f)
-#define SL(a) scanf("%lld", &a)
-#define SLL(a, b) scanf("%lld%lld", &a, &b)
-#define SLLL(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
-#define SLLLL(a, b, c, d) scanf("%lld%lld%lld%lld", &a, &b, &c, &d)
-#define SD(a) scanf("%lf", &a)
-#define SDD(a, b) scanf("%lf%lf", &a, &b)
-#define SDDD(a, b, c) scanf("%lf%lf%lf", &a, &b, &c)
-#define SDDDD(a, b, c, d) scanf("%lf%lf%lf%lf", &a, &b, &c, &d)
-#define SA(a, i, n) For(i, n) scanf("%d", a + i) /// 便于扩展 *不要在后面加逗号！
-#define SAA(a, i, n, j, m) For(i, n) For(j, m) SI(a[i][j]) /// *不要在后面加逗号！
+#define SI(s) scanf("%d", &s)
+#define SII(s, b) scanf("%d%d", &s, &b)
+#define SIII(s, b, c) scanf("%d%d%d", &s, &b, &c)
+#define SIIII(s, b, c, d) scanf("%d%d%d%d", &s, &b, &c, &d)
+#define SIIIII(s, b, c, d, e) scanf("%d%d%d%d%d", &s, &b, &c, &d, &e)
+#define SIIIIII(s, b, c, d, e, f) scanf("%d%d%d%d%d%d", &s, &b, &c, &d, &e, &f)
+#define SL(s) scanf("%lld", &s)
+#define SLL(s, b) scanf("%lld%lld", &s, &b)
+#define SLLL(s, b, c) scanf("%lld%lld%lld", &s, &b, &c)
+#define SLLLL(s, b, c, d) scanf("%lld%lld%lld%lld", &s, &b, &c, &d)
+#define SD(s) scanf("%lf", &s)
+#define SDD(s, b) scanf("%lf%lf", &s, &b)
+#define SDDD(s, b, c) scanf("%lf%lf%lf", &s, &b, &c)
+#define SDDDD(s, b, c, d) scanf("%lf%lf%lf%lf", &s, &b, &c, &d)
+#define SA(s, i, n) For(i, n) scanf("%d", s + i) /// 便于扩展 *不要在后面加逗号！
+#define SAA(s, i, n, j, m) For(i, n) For(j, m) SI(s[i][j]) /// *不要在后面加逗号！
 #define GC(c) (c = getchar())
 #define GCn() getchar()
 #define UC(c) ungetc(c, stdin)
 #define SS(s) scanf("%s", s)
 #define SSS(s, s2) scanf("%s%s", s, s2)
+#define SC(c) scanf(" %c", &c)
 
-typedef multiset<int>::iterator msiter;
-typedef multimap<int, int>::iterator mmter;
-#define PI(a) printf("%d\n", a)
-#define PII(a, b) printf("%d %d\n", a, b)
-#define PIII(a, b, c) printf("%d %d %d\n", a, b, c)
-#define PIIII(a, b, c, d) printf("%d %d %d %d\n", a, b, c, d)
-#define PL(a) printf("%lld\n", a)
-#define PLL(a, b) printf("%lld %lld\n", a, b)
-#define PLLL(a, b, c) printf("%lld %lld %lld\n", a, b, c)
-#define PD(a) printf("%f\n", a)
-#define PDD(a, b) printf("%f %f\n", a, b)
-#define PDDD(a, b, c) printf("%f %f %f\n", a, b, c)
-#define PA(a, i, n) For(i, (n) - 1) printf("%d ", a[i]); PI(a[(n) - 1]) /// *(有时要在前面加花括号)由于要支持STL的数据类型，故不用+的形式，必要时请手动改成+
-#define PAA(a, i, n, j, m) For(i, n) {For(j, (m) - 1) printf("%d ", a[i][j]); PI(a[i][(m) - 1]);}
-#define PAn(a, i, n) For(i, n) PI(a[i])
-#define PiA(a, i, n) For(i, n) PII(i, a[i]) /// debug用
-#define rPA(a, i, n) rForr(i, n - 1, 1) printf("%d ", a[i]); PI(a[0]) /// *(有时要在前面加花括号)
-#define rPAn(a, i, n) rFor(i, n - 1) PI(a[i])
+#define PI(s) printf("%d\n", s)
+#define PII(s, b) printf("%d %d\n", s, b)
+#define PIII(s, b, c) printf("%d %d %d\n", s, b, c)
+#define PIIII(s, b, c, d) printf("%d %d %d %d\n", s, b, c, d)
+#define PIIIII(s, b, c, d, e) printf("%d %d %d %d %d\n", s, b, c, d, e)
+#define PL(s) printf("%lld\n", s)
+#define PLL(s, b) printf("%lld %lld\n", s, b)
+#define PLLL(s, b, c) printf("%lld %lld %lld\n", s, b, c)
+#define PD(s) printf("%f\n", s)
+#define PDD(s, b) printf("%f %f\n", s, b)
+#define PDDD(s, b, c) printf("%f %f %f\n", s, b, c)
+#define PA(s, i, n) For(i, (n) - 1) printf("%d ", s[i]); PI(s[(n) - 1]) /// *(有时要在前面加花括号)由于要支持STL的数据类型，故不用+的形式，必要时请手动改成+
+#define PAA(s, i, n, j, m) For(i, n) {For(j, (m) - 1) printf("%d ", s[i][j]); PI(s[i][(m) - 1]);}
+#define PAn(s, i, n) For(i, n) PI(s[i])
+#define PiA(s, i, n) For(i, n) PII(i, s[i]) /// debug用
+#define rPA(s, i, n) rForr(i, n - 1, 1) printf("%d ", s[i]); PI(s[0]) /// *(有时要在前面加花括号)
+#define rPAn(s, i, n) rFor(i, n - 1) PI(s[i])
 #define PC(c) putchar(c)
 #define Pn() putchar(10)
 #define Ps() putchar(32)
 
-#define FI freopen("in.txt", "r", stdin)
-#define FO freopen("out.txt", "w", stdout)
-#define Fin(f) freopen(f, "r", stdin) /// 请手动加.in
-#define Fout(f) freopen(f, "w", stdout) /// 请手动加.out
-#define random(m) ((rand() << 16 | rand()) % m) /// [0,m)之间的伪随机数
-#define SR() srand((unsigned)time(NULL))
+#define Uni(s) s.resize(unique(all(s)) - s.begin()) /// STL专用
+#define SUni(s) sort(all(s)); Uni(s) /// STL专用
+#define Unii(s, n) (unique(s, s + (n)) - s)
+#define SUnii(s, n) sort(s, s + n); Unii(s, n)
+#define Acc(s, n) (accumulate(s, s + (n), 0)) /// 注意0LL!!!!!以及0.0!!!
+#define AaddB(s, n, b) transform(s, s + (n), b, s, plus<int>()) /// *慢的话就改为For(i, n) s[i] += b[i](注意加int i)
+#define mem(s, num) memset(s, num, sizeof(s))
+#define cpy(to, from) memcpy(to, from, sizeof(from))
+#define Rcpy(l, r, b) reverse_copy(l, r, b) /// 注意为左闭右开区间
 
-typedef long double ld;
+#define gr() greater<int>()
+#define nth(s, k, n) nth_element(s + 0, s + k, s + n) /// *可能要事先--k
+#define nthg(s, k, n) nth_element(s + 0, s + k, s + n, greater<int>()) /// *可能要事先--k
+#define Min(s, n) (*min_element(s, s + (n)))
+#define Max(s, n) (*max_element(s, s + (n)))
+#define Minpos(s, n) (min_element(s, s + (n)) - (s))
+#define Maxpos(s, n) (max_element(s, s + (n)) - (s))
+#define Lowpos(s, n, x) (lower_bound(s, s + (n), x) - (s))
+#define Upppos(s, n, x) (upper_bound(s, s + (n), x) - (s))
+#define BS(s, n, x) binary_search(s, s + (n), x) /// 返回bool值
+#define Range(s, n, x) equal_range(s, s + (n), x) /// 返回pair
+#define Fpos(s, n, x) (find(s, s + (n), x) - (s))
+#define Fd(s, x) (find(all(s),x) != s.end())
+
 const int inf = 0x3f3f3f3f; /// 1.06e9 (INT_MAX为2.147e9)
 const long long llinf = 0x3f3f3f3f3f3f3f3fLL; /// 4.56e18 (LLONG_MAX为9.22e18)
 const double pi = acos(-1.0);
 //const double tens[11] = {0.0, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10};
-//double fgcd(double a, double b) {return fabs(b) > eps ? fgcd(b, fmod(a, b)) : a;}
+//double fgcd(double s, double b) {return fabs(b) > eps ? fgcd(b, fmod(s, b)) : s;}
 
 //const int dir[4][2] = {1, 0, 0, 1, -1, 0, 0, -1};
 //const int dirr[8][2] = {1, 0, 1, 1, 0, 1, -1, 1, -1, 0, -1, -1, 0, -1, 1, -1};
@@ -128,7 +134,7 @@ const double pi = acos(-1.0);
 /// 如果用gets(s), GC(ch)读入WA的话，请用SS(s), scanf(" %c ", &ch)代替
 /// 在main()中大量初始化STL类型容易死机
 /// 注意strncpy不会加尾0，请手动添加
-/// 相对位置不变的排序stable_sort(a, a + n);
+/// 相对位置不变的排序stable_sort(s, s + n);
 /// C++会帮你往上类型转换，但G++不会
 /*G++扩栈
 int __size__ = 256 << 20; // 256MB
@@ -137,58 +143,60 @@ __asm__("movl %0, %%esp\n" :: "r"(__p__));
 */
 //ios_base::sync_with_stdio(false);
 
-typedef pair<ll, ll> p2; /// 赋值时直接SII(a[i].x, a[i].y)就行, 有时候用LL
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int>::iterator viter;
+typedef set<ll>::iterator siter;
+typedef map<int, int>::iterator miter;
+typedef multiset<int>::iterator msiter;
+typedef multimap<int, int>::iterator mmter;
+typedef priority_queue<int> pqi;
+typedef pair<int, int> p2; /// 赋值时直接SII(s[i].x, s[i].y)就行, 有时候用LL
 typedef pair<pair<int, int>, int> p3;
 typedef pair<int, pair<int, int> > pi3;
-#define x first
-#define y second
-//#define MT(a, b, c) make_pair(make_pair(a, b), c)
+//#define x first
+//#define y second
+//#define MT(s, b, c) make_pair(make_pair(s, b), c)
 
-typedef priority_queue<int> pqi;
-const double eps = 1e-6;
-//const ll mod = ll(1e9) + 7;
+//const double eps = 1e-8;
+//const ll mod = ll(1e9) + 7; /// *或int
 #define Pcas() printf("Case %d: ", ++cas) /// *注意C的大小写
-const int mx = int(1e3) + 5;
 
-ll xx, yy;
-vector<p2> p;
+const int mxn = int(1e9);
 
-void init()
+set<ll> s;
+
+inline ll f(ll a, ll b)
 {
-    int n;
-	ll xi, yi;
-	SLL(xx, yy), p.PB(MP(xx, yy));
-	SI(n);
-	while (n--) SLL(xi, yi), p.PB(MP(xi, yi));
+	return a * b + ((a + b + 1) << 1);
 }
 
-bool ok(vector<p2> &p)
+void init_closure(ll a0, ll a1)
 {
-	SUni(p);
-	int n = p.size();
-	if (n == 1) return true;
-	if (p[0] != MP(xx, yy) && p.back() != MP(xx, yy)) return false; /// 出发点不在开头结尾
-	if (p.back() == MP(xx, yy)) reverse(all(p));
-	int i;
-	For(i, n - 1) if (p[i].x == p[i + 1].x) return false;
-	ll a1 = sq(p[1].x) - sq(xx), b1 = p[1].x - xx, c1 = p[1].y - yy;
-	ll a2 = sq(p[2].x) - sq(xx), b2 = p[2].x - xx, c2 = p[2].y - yy;
-	/// *如果a2就已经很大了，那下面还是改为double吧
-	ll d = a1 * b2 - a2 * b1, d1 = -(c2 * b1 - c1 * b2), d2 = a1 * c2 - a2 * c1;
-	if (d == 0 || d1 == 0 || d < 0 && d1 < 0 || d > 0 && d1 > 0) return false;
-	double tmpp, aa, bb, cc;
-	Forr(i, 3, n)
+	s.insert(1LL), s.insert(3LL);
+	int i, j, tmpsz;
+	ll a, tmp;
+	siter it;
+	for (i = 0;; ++i)
 	{
-		aa = sq(p[i].x) - sq(xx), bb = p[i].x - xx, cc = p[i].y - yy;
-		tmpp = aa * d1 + bb * d2 - cc * d;
-		if (fabs(tmpp) > eps) return false;
+		for (it = s.begin(), j = 0; it != s.end(), j < i; ++it, ++j) ;
+		if (it == s.end()) break;
+		a = *it;
+		tmpsz = s.size();
+		loop(s)
+		{
+			tmp = f(a, *it);
+			if (tmp > mxn) break;
+			s.insert(tmp);
+		}
+		if (s.size() == tmpsz) break; /// 未发生修改
 	}
-	return true;
 }
 
+#define IO /// *别忘了删掉!
 int main()
 {
-	init();
-	puts(ok(p) ? "Yes" : "No");
+
 	return 0;
 }
