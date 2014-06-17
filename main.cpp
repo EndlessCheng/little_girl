56dde086cde4ace484b6346f11e2a606f98c1bfe
endlesscheng <loli.con@qq.com>
2014-06-17 11:23:27 +0800

    UVa 10219

--- main.cpp
+++ main.cpp
@@ -1,64 +1,18 @@
-#include<stdio.h>
-#include<string.h>
-#include<algorithm>
-#include<math.h>
-#include<iostream>
-using namespace std;
-int a,b,c,d,e,f;
-int n,k;
-int now,nex;
-int arr[1005];
-int vis[1005];
-int suan(int i)
+#include<cstdio>
+#include<cmath>
+
+inline double len(int n)
 {
-	if (i>n) return i-n;
-	else return i;
+	return 0.5 * log10(2 * M_PI * n) + n * log10((double)n) - n / log(10);
 }
+
+inline int len(int n, int k)
+{
+	return int(len(n) - len(k) - len(n - k)) + 1;
+}
+
 int main()
 {
-	while (cin>>n>>k)
-	{
-		if (n==k && n==0) break;
-		now=1;
-		now=suan(now+n-1);
-		memset(vis,0,sizeof(vis));
-		for (a=1;a<=n;a++) arr[a]=a;
-		for (a=n;a>1;a--) //还剩这么多人
-		{
-			/*for (b=1;b<=n;b++)
-			if (vis[arr[b]]==0)
-			{
-				cout<<arr[b]<<' ';
-			}
-			cout<<endl;*/
-			int lef=k;
-
-			int len=0;
-			while (len<lef)
-			{
-				now=suan(now+1);
-				if (vis[arr[now]]==0) len++;
-			}
-			//杀掉这个人
-			//cout<<arr[now]<<endl;
-			vis[arr[now]]=1;
-			len=0;
-
-			int pre=now; //记住now这个位置
-			while (len<lef)
-			{
-				now=suan(now+1);
-				if (vis[arr[now]]==0) len++;
-			}
-			swap(arr[pre],arr[now]);
-			//arr[pre]=arr[now]; //把那个人换过来
-			now=pre;
-		}
-		for (a=1;a<=n;a++)
-			if (vis[arr[a]]==0)
-		{
-			int wei=suan(n+1-(arr[a]-1));
-			cout<<wei<<endl;
-		}
-	}
+	int n, k;
+	while (~scanf("%d%d", &n, &k)) printf("%d\n", len(n, k));
 }
