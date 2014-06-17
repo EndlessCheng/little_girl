56e398556d859edfcbda75335ad30627c79d5561
endlesscheng <loli.con@qq.com>
2014-06-17 12:51:53 +0800

    UVa 821

--- main.cpp
+++ main.cpp
@@ -1,54 +1,70 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iomanip>
+#include <utility>
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+using namespace std;
 
-int trans(char num[],int k)
+const int SZ = 111;
+const int INF = 0x3f;
+
+int dis[SZ][SZ];
+map<int, int> mp;
+int main(int arc, char const *argv[])
 {
-  int ans=0,len=strlen(num);
-  int i;
-  for(i=0;i<len;i++)
-  if(num[i]>='0'&&num[i]<='9')
-  {
-	if(num[i]-'0'>=k)return -1;
-	else
-	ans=ans*k+num[i]-'0';
-  }
-  else if(num[i]-'A'+10>=k)return -1;
-  else ans=ans*k+num[i]-'A'+10;
- return ans;
-}
+    ios::sync_with_stdio(false);
+    int u, v, nxt, kase, i, j, k;
+    double sum;
+    kase=nxt=0;
+    while(cin>>u>>v and (u+v)) {
+        nxt=0;
+        memset(dis,INF,sizeof(dis));
+        mp.clear();
+        for(i=0; i<SZ; ++i) dis[i][i] = 0;
+        if(mp.find(u)==mp.end()) {
+            mp.insert(pair<int, int>(u,nxt));
+            nxt++;
+        }
+        if(mp.find(v)==mp.end()) {
+            mp.insert(pair<int, int>(v,nxt));
+            nxt++;
+        }
+        dis[mp[u]][mp[v]]= 1;
+        cout<<"Case "<<++kase<<":";
+        while(cin>>u>>v and (u+v)) {
+            if(mp.find(u)==mp.end()) {
+                mp.insert(pair<int, int>(u,nxt));
+                nxt++;
+            }
+            if(mp.find(v)==mp.end()) {
+                mp.insert(pair<int, int>(v,nxt));
+                nxt++;
+            }
+            dis[mp[u]][mp[v]] = 1;
+        }
 
-
-
-int main()
-{
-char x[100],y[100];
-int bx,by,flag,x1,y1;
-int i,j;
-
-while(scanf("%s%s",x,y)!=EOF)
-{
-   flag=0;
-   for(i=2;i<=36;i++)
-   if(!flag)
-   {
-    x1=trans(x,i);
-	if(x1<0)continue;
-    for(j=2;j<=36;j++)
-	if(!flag)
-	{
-	 y1=trans(y,j);
-	 if(y1<0)continue;
-	  if(x1==y1)
-	 {
-	  bx=i;by=j;
-	  flag=1;
-	 }
-	}
-   }
-   if(flag)printf("%s (base %d) = %s (base %d)\n",x,bx,y,by);
-   else printf("%s is not equal to %s in any base 2..36\n",x,y);
-}
-return 0;
+        for (i=0; i<nxt; ++i) {
+            for (j=0; j<nxt; ++j) {
+                for (k=0; k<nxt; ++k) {
+                    dis[j][k] = min(dis[j][k], dis[j][i]+dis[i][k]);
+                }
+            }
+        }
+        sum = 0.0;
+        for(i=0; i<nxt; ++i) {
+            for(j=0; j<nxt; ++j) {
+                    sum+=dis[i][j];
+            }
+        }
+        sum/=(double)((nxt-1)*nxt);
+        cout<<" average length between pages = "<<fixed<<setprecision(3)<<sum<<" clicks"<<endl;
+    }
+    return 0;
 }
