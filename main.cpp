23e1aa30d3028ab7ec0dedd0ddde86ccba5fbdc1
endlesscheng <loli.con@qq.com>
2014-06-17 10:32:07 +0800

    UVa 130 约瑟夫环

--- main.cpp
+++ main.cpp
@@ -1,35 +1,64 @@
 #include<stdio.h>
-#include<vector>
-
+#include<string.h>
+#include<algorithm>
+#include<math.h>
+#include<iostream>
 using namespace std;
-
-vector<int> pset;
-void initSet(int N) {
-    pset.assign(N,0);
-    for (int i = 0; i < N; ++i) pset[i] = i;
+int a,b,c,d,e,f;
+int n,k;
+int now,nex;
+int arr[1005];
+int vis[1005];
+int suan(int i)
+{
+	if (i>n) return i-n;
+	else return i;
 }
-int findSet(int i) { return (pset[i] == i) ? i : (pset[i] = findSet(pset[i])); }
-void unionSet(int i, int j) { pset[findSet(i)] = findSet(j); }
-int numDisjointSets() {
-    int num = 0;
-    for(int i = 0; i < pset.size(); ++i) { if(i == findSet(i)) ++num;
-    }
-    return num;
-}
+int main()
+{
+	while (cin>>n>>k)
+	{
+		if (n==k && n==0) break;
+		now=1;
+		now=suan(now+n-1);
+		memset(vis,0,sizeof(vis));
+		for (a=1;a<=n;a++) arr[a]=a;
+		for (a=n;a>1;a--) //还剩这么多人
+		{
+			/*for (b=1;b<=n;b++)
+			if (vis[arr[b]]==0)
+			{
+				cout<<arr[b]<<' ';
+			}
+			cout<<endl;*/
+			int lef=k;
 
-int main(){
-    int n, m;
-    int i, j;
-    int c = 0;
-    while(scanf("%d %d", &n, &m) != EOF && n != 0)
-    {
-        initSet(n);
-        for(int k = 0; k < m; ++k)
-        {
-            scanf("%d %d", &i, &j);
-            unionSet(i,j);
-        }
-        printf("Case %d: %d\n", ++c, numDisjointSets());
-        pset.clear();
-    }
+			int len=0;
+			while (len<lef)
+			{
+				now=suan(now+1);
+				if (vis[arr[now]]==0) len++;
+			}
+			//杀掉这个人
+			//cout<<arr[now]<<endl;
+			vis[arr[now]]=1;
+			len=0;
+
+			int pre=now; //记住now这个位置
+			while (len<lef)
+			{
+				now=suan(now+1);
+				if (vis[arr[now]]==0) len++;
+			}
+			swap(arr[pre],arr[now]);
+			//arr[pre]=arr[now]; //把那个人换过来
+			now=pre;
+		}
+		for (a=1;a<=n;a++)
+			if (vis[arr[a]]==0)
+		{
+			int wei=suan(n+1-(arr[a]-1));
+			cout<<wei<<endl;
+		}
+	}
 }
