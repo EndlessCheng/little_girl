304745514f365a600a436cddcaf3e2fcf3a83685
endlesscheng <loli.con@qq.com>
2014-06-17 09:51:27 +0800

    UVa 10409

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
 
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+int main(){
+	int q,i;
+	char c[20];
+	while(cin>>q&&q){
+		int m=1,n=2,s=5,e=4,w=3,o=6,t;
+		for(i=0;i<q;i++){
+			scanf("%s",c);
+		if(strcmp(c,"north")==0)
+		{t=n;n=m;m=s;s=o;o=t;}
+		else if(strcmp(c,"south")==0)
+		{t=s;s=m;m=n;n=o;o=t;}
+		else if(strcmp(c,"east")==0)
+		{t=e;e=m;m=w;w=o;o=t;}
+		else{t=w;w=m;m=e;e=o;o=t;}
+		}
+		cout<<m<<endl;
+	}
+	return 0;
+}
