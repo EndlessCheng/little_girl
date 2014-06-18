da5b835f9aaffe8fa092b7bcf616fe4f715d788e
endlesscheng <loli.con@qq.com>
2014-06-18 18:51:32 +0800

    UVa 213

--- main.cpp
+++ main.cpp
@@ -1,46 +1,203 @@
-#include <stdio.h>
-#include <string.h>
+// Author: Jo�o Xavier
+// Date: 26-04-2012
+
+/* @JUDGE_ID: '00000' 213 C++ "Message Decoding" */
+
+/* @BEGIN_OF_SOURCE_CODE */
+
 #include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <string.h>
+#include <sstream>
+
+#define MAX_LENGTH 115
+#define ADVANCE 3
+
+// states
+enum state {
+    STATE_RD_HEADER,
+    STATE_RD_MSG_SIZE,
+    STATE_RD_CODE,
+    STATE_PRINT
+};
 
 using namespace std;
-const int N = int(1e6)+5;
-const int INF = 0x3f3f3f3f;
 
-struct state {
-	int c, f;
-}s[N];
-int n;
+unsigned int bin2dec[MAX_LENGTH];
+unsigned int binMaxs[] = { 0, 1, 3, 7, 15, 31, 63, 127 };
 
-void init () {
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) scanf("%d", &s[i].f);
-	for (int i = 0; i < n; i++) scanf("%d", &s[i].c);
-	int tmp = INF;
-	for (int i = n - 1; i >= 0; i--) {
-		tmp = min(tmp, s[i].c);
-		tmp = max(tmp, s[i].f);
-		s[i].c = tmp;
-	}
+inline int stoi(const string str) {
+    stringstream ss(str);
+    int i;
+    ss >> i;
+    return i;
 }
 
-int solve () {
-	int ans = 0, tmp = INF;
-	for (int i = 0; i < n; i++) {
-		tmp = max(tmp, s[i].f);
-		tmp = min(tmp, s[i].c);
-		s[i].c = tmp;
-
-		ans += s[i].c - s[i].f;
-	}
-	return ans;
+inline bool isMsg(const string str) {
+    return str.find('1') != string::npos;
 }
 
-int main () {
-	int cas;
-	scanf("%d", &cas);
-	while (cas--) {
-		init();
-		printf("%d\n", solve());
-	}
-	return 0;
+inline bool isEndCode(const string str) {
+    return str.find('0') == string::npos;
 }
+
+string dec2bin(unsigned n, unsigned int length) {
+    const int size = sizeof(n)*8;
+    string res;
+    bool s = false;
+    for (int a = 0; a < size; a++) {
+        bool bit = n >> (size-1);
+        if (bit)
+            s = true;
+        if (s)
+            res.push_back(bit + '0');
+        n<<=1;
+    }
+    if (!res.size())
+        res.push_back('0');
+
+    while (res.size() < length) {
+        res = "0" + res;
+    }
+
+    return res;
+}
+
+int main (int argc, char* argv[]) {
+    // init array
+    memset(bin2dec, 0, sizeof(bin2dec));
+    bin2dec[1] =   1;
+    bin2dec[10] =  2;
+    bin2dec[11] =  3;
+    bin2dec[100] = 4;
+    bin2dec[101] = 5;
+    bin2dec[110] = 6;
+    bin2dec[111] = 7;
+
+    map<string, char> decode;
+    string lineBuffer = "", header, pool, decodedMsg;
+    state currentState = STATE_RD_HEADER;
+
+    unsigned int msgSize = 0;
+    bool shouldRepeat = false;
+
+    while (!cin.eof()) {
+        getline(cin, lineBuffer);
+
+        // clean input
+        size_t found;
+        do {
+            found = lineBuffer.find('\r');
+
+            if (found != string::npos) {
+                lineBuffer.erase(found, 1);
+            } else {
+                break;
+            }
+        }
+        while (found != string::npos);
+
+        if (lineBuffer.length() == 0) {
+            continue; // ignore all empty lines
+        }
+        else {
+            do {
+                switch (currentState) {
+                    case STATE_RD_HEADER: {
+                        // reinit vars
+                        pool = "";
+                        decodedMsg = "";
+                        msgSize = 0;
+                        decode.clear();
+
+                        header = lineBuffer;
+                        const unsigned int headerLength = header.length();
+
+                        // fill map
+                        for (unsigned int counter = 0, binLength = 1; counter < headerLength; binLength++) {
+                            unsigned int max = binMaxs[binLength];
+
+                            for (unsigned int i = 0; i < max && counter < headerLength; i++, counter++) {
+                                decode[dec2bin(i, binLength)] = header[counter];
+                            }
+                        }
+
+                        // state advance
+                        shouldRepeat = false;
+                        currentState = STATE_RD_MSG_SIZE;
+                        break;
+                    }
+
+                    case STATE_RD_MSG_SIZE: {
+                        unsigned int fixedAdvance = ADVANCE - pool.length();
+                        pool += lineBuffer.substr(0, fixedAdvance);
+
+                        if (pool.length() == ADVANCE) {
+                            msgSize = bin2dec[stoi(pool)];
+
+                            pool = "";
+                            lineBuffer = lineBuffer.substr(fixedAdvance);
+
+                            // state advance
+                            currentState = (msgSize ? STATE_RD_CODE : STATE_PRINT);
+                            shouldRepeat = true;
+                        } else {
+                            shouldRepeat = false;
+                        }
+
+                        break;
+                    }
+
+                    case STATE_RD_CODE: {
+                        pool += lineBuffer;
+                        string code;
+
+                        for (unsigned int idx = 0; currentState == STATE_RD_CODE; idx += msgSize) {
+                            code = pool.substr(idx, msgSize);
+
+                            if (code.length() != msgSize) {
+                                pool = code;
+                                shouldRepeat = false;
+                                break;
+                            }
+
+                            if (isEndCode(code)) {
+                                if (idx + msgSize >= pool.length()) {
+                                    lineBuffer = "";
+                                    shouldRepeat = false;
+                                }
+                                else {
+                                    lineBuffer = pool.substr(idx + msgSize);
+                                    shouldRepeat = true;
+                                }
+
+                                // state advance
+                                pool = "";
+                                currentState = STATE_RD_MSG_SIZE;
+                            } else {
+                                decodedMsg += decode[code];
+                            }
+                        }
+
+                        break;
+                    }
+
+                    case STATE_PRINT: {
+                        cout << decodedMsg << endl;
+
+                        // state advance
+                        shouldRepeat = false;
+                        currentState = STATE_RD_HEADER;
+                        break;
+                    }
+                }
+            } while (shouldRepeat);
+        }
+    }
+
+    return 0;
+}
+
+/* @END_OF_SOURCE_CODE */
