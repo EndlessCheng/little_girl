1d1e000d989a2d03e14ed458d01bd826b7aa4a62
endlesscheng <loli.con@qq.com>
2014-06-18 18:57:43 +0800

    UVa 1589

--- main.cpp
+++ main.cpp
@@ -1,203 +1,142 @@
-// Author: Jo�o Xavier
-// Date: 26-04-2012
-
-/* @JUDGE_ID: '00000' 213 C++ "Message Decoding" */
-
-/* @BEGIN_OF_SOURCE_CODE */
-
-#include <algorithm>
-#include <iostream>
-#include <map>
-#include <string>
-#include <string.h>
-#include <sstream>
-
-#define MAX_LENGTH 115
-#define ADVANCE 3
-
-// states
-enum state {
-    STATE_RD_HEADER,
-    STATE_RD_MSG_SIZE,
-    STATE_RD_CODE,
-    STATE_PRINT
-};
-
-using namespace std;
-
-unsigned int bin2dec[MAX_LENGTH];
-unsigned int binMaxs[] = { 0, 1, 3, 7, 15, 31, 63, 127 };
-
-inline int stoi(const string str) {
-    stringstream ss(str);
-    int i;
-    ss >> i;
-    return i;
+#include<stdio.h>
+#include<string.h>
+int n,x0,y0,mark[10][10],die[10][10];
+int kk[8][2]={-2,-1,-2,1,-1,2,1,2,2,1,2,-1,1,-2,-1,-2};
+int s1[4][2]={-1,0,0,1,1,0,0,-1};
+struct node
+{
+    int x,y;
+    char c;
+}arr[20];
+int judge1(int x,int y)
+{
+    if (x<0||x>9||y<0||y>8) return 0;
+	return 1;
 }
-
-inline bool isMsg(const string str) {
-    return str.find('1') != string::npos;
+int judge2(int x,int y)
+{
+    if (x<0||x>2||y<3||y>5) return 0;
+	return 1;
 }
-
-inline bool isEndCode(const string str) {
-    return str.find('0') == string::npos;
-}
-
-string dec2bin(unsigned n, unsigned int length) {
-    const int size = sizeof(n)*8;
-    string res;
-    bool s = false;
-    for (int a = 0; a < size; a++) {
-        bool bit = n >> (size-1);
-        if (bit)
-            s = true;
-        if (s)
-            res.push_back(bit + '0');
-        n<<=1;
+void work(node arr)
+{
+	int i,j;
+	if(arr.c=='H')
+    {
+        for (i=0;i<8;i++)
+		{
+            if (judge1(arr.x+s1[i/2][0],arr.y+s1[i/2][1]))
+			{
+                if (!mark[arr.x+s1[i/2][0]][arr.y+s1[i/2][1]])
+                {
+                    if (judge1(arr.x+kk[i][0],arr.y+kk[i][1])) die[arr.x+kk[i][0]][arr.y+kk[i][1]]=1;
+                }
+			}
+		}
     }
-    if (!res.size())
-        res.push_back('0');
-
-    while (res.size() < length) {
-        res = "0" + res;
+    else if((arr.c=='G')||(arr.c=='R'))
+    {
+        for (i=arr.x+1;i<10;i++)
+        {
+            die[i][arr.y]=1;
+            if(mark[i][arr.y]==1) break;
+        }
+        for (i=arr.x-1;i>=0;i--)
+        {
+            die[i][arr.y]=1;
+            if (mark[i][arr.y]==1) break;
+        }
+        for (i=arr.y+1;i<9;i++)
+        {
+            die[arr.x][i]=1;
+            if(mark[arr.x][i]==1) break;
+        }
+        for (i=arr.y-1;i>=0;i--)
+        {
+            die[arr.x][i]=1;
+            if (mark[arr.x][i]==1) break;
+        }
     }
-
-    return res;
-}
-
-int main (int argc, char* argv[]) {
-    // init array
-    memset(bin2dec, 0, sizeof(bin2dec));
-    bin2dec[1] =   1;
-    bin2dec[10] =  2;
-    bin2dec[11] =  3;
-    bin2dec[100] = 4;
-    bin2dec[101] = 5;
-    bin2dec[110] = 6;
-    bin2dec[111] = 7;
-
-    map<string, char> decode;
-    string lineBuffer = "", header, pool, decodedMsg;
-    state currentState = STATE_RD_HEADER;
-
-    unsigned int msgSize = 0;
-    bool shouldRepeat = false;
-
-    while (!cin.eof()) {
-        getline(cin, lineBuffer);
-
-        // clean input
-        size_t found;
-        do {
-            found = lineBuffer.find('\r');
-
-            if (found != string::npos) {
-                lineBuffer.erase(found, 1);
-            } else {
+    else if(arr.c=='C')
+    {
+        for(i=arr.x+1;i<10;i++)
+		{
+            if(mark[i][arr.y]==1)
+            {
+                for(j=i+1;j<10;j++)
+                {
+                    die[j][arr.y]=1;
+                    if(mark[j][arr.y]==1) break;
+                }
                 break;
             }
-        }
-        while (found != string::npos);
-
-        if (lineBuffer.length() == 0) {
-            continue; // ignore all empty lines
-        }
-        else {
-            do {
-                switch (currentState) {
-                    case STATE_RD_HEADER: {
-                        // reinit vars
-                        pool = "";
-                        decodedMsg = "";
-                        msgSize = 0;
-                        decode.clear();
-
-                        header = lineBuffer;
-                        const unsigned int headerLength = header.length();
-
-                        // fill map
-                        for (unsigned int counter = 0, binLength = 1; counter < headerLength; binLength++) {
-                            unsigned int max = binMaxs[binLength];
-
-                            for (unsigned int i = 0; i < max && counter < headerLength; i++, counter++) {
-                                decode[dec2bin(i, binLength)] = header[counter];
-                            }
-                        }
-
-                        // state advance
-                        shouldRepeat = false;
-                        currentState = STATE_RD_MSG_SIZE;
-                        break;
-                    }
-
-                    case STATE_RD_MSG_SIZE: {
-                        unsigned int fixedAdvance = ADVANCE - pool.length();
-                        pool += lineBuffer.substr(0, fixedAdvance);
-
-                        if (pool.length() == ADVANCE) {
-                            msgSize = bin2dec[stoi(pool)];
-
-                            pool = "";
-                            lineBuffer = lineBuffer.substr(fixedAdvance);
-
-                            // state advance
-                            currentState = (msgSize ? STATE_RD_CODE : STATE_PRINT);
-                            shouldRepeat = true;
-                        } else {
-                            shouldRepeat = false;
-                        }
-
-                        break;
-                    }
-
-                    case STATE_RD_CODE: {
-                        pool += lineBuffer;
-                        string code;
-
-                        for (unsigned int idx = 0; currentState == STATE_RD_CODE; idx += msgSize) {
-                            code = pool.substr(idx, msgSize);
-
-                            if (code.length() != msgSize) {
-                                pool = code;
-                                shouldRepeat = false;
-                                break;
-                            }
-
-                            if (isEndCode(code)) {
-                                if (idx + msgSize >= pool.length()) {
-                                    lineBuffer = "";
-                                    shouldRepeat = false;
-                                }
-                                else {
-                                    lineBuffer = pool.substr(idx + msgSize);
-                                    shouldRepeat = true;
-                                }
-
-                                // state advance
-                                pool = "";
-                                currentState = STATE_RD_MSG_SIZE;
-                            } else {
-                                decodedMsg += decode[code];
-                            }
-                        }
-
-                        break;
-                    }
-
-                    case STATE_PRINT: {
-                        cout << decodedMsg << endl;
-
-                        // state advance
-                        shouldRepeat = false;
-                        currentState = STATE_RD_HEADER;
-                        break;
-                    }
-                }
-            } while (shouldRepeat);
-        }
+		}
+		for (i=arr.x-1;i>=0;i--)
+		{
+			if(mark[i][arr.y]==1)
+			{
+				for(j=i-1;j>=0;j--)
+				{
+					die[j][arr.y]=1;
+					if(mark[j][arr.y]==1) break;
+				}
+				break;
+			}
+		}
+		for (i=arr.y+1;i<9;i++)
+		{
+			if (mark[arr.x][i]==1)
+			{
+				for (j=i+1;j<9;j++)
+				{
+					die[arr.x][j]=1;
+					if(mark[arr.x][j]==1) break;
+				}
+				break;
+			}
+		}
+		for (i=arr.y-1;i>=0;i--)
+		{
+			if (mark[arr.x][i]==1)
+			{
+				for (j=i-1;j>=0;j--)
+				{
+					die[arr.x][j]=1;
+					if (mark[arr.x][j]==1) break;
+				}
+				break;
+			}
+		}
     }
-
-    return 0;
 }
-
-/* @END_OF_SOURCE_CODE */
+int main()
+{
+	int i,a,b;
+	char ss[5];
+    while(~scanf("%d%d%d",&n,&x0,&y0))
+    {
+		memset(mark,0,sizeof(mark));
+        memset(die,0,sizeof(die));
+        if (n==0&&x0==0&&y0==0) break;
+        x0--;y0--;
+        for(i=0;i<n;i++)
+		{
+			scanf("%s %d %d",ss,&a,&b);
+			arr[i].c =ss[0];
+			arr[i].x=a-1;arr[i].y=b-1;
+			mark[arr[i].x][arr[i].y]=1;
+		}
+        for(i=0;i<n;i++) work(arr[i]);
+        int flag=0;
+		for (i=0;i<4;i++)
+		{
+            if (judge2(x0+s1[i][0],y0+s1[i][1]))
+			{
+                if (!die[x0+s1[i][0]][y0+s1[i][1]]) flag=1;
+			}
+		}
+		if (flag) puts("NO");
+		else puts("YES");
+    }
+	return 0;
+}
