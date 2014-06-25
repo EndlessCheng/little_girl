1fad9699b50a7a379965e37e4c75fd6845aab168
EndlessCheng <loli.con@qq.com>
2014-06-25 10:28:39 +0800

    UVa 10199

--- main.cpp
+++ main.cpp
@@ -1,65 +1,77 @@
-#include<iostream>
-#include<cstdio>
-#include<vector>
-#include<map>
-#include<algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <sstream>
 
 using namespace std;
 
+typedef long long ll;
 
-struct data
-{
-    char suit,card;
-};
+#define REP(i,s,t) for(int i=(s);i<(t);i++)
+#define FOREACH(i,v) for(typeof((v).begin()) i=(v).begin();i!=(v).end();i++)
+#define FILL(x,v) memset(x,v,sizeof(x))
+const int INF = (int)1E9;
+#define MAXN 105
 
-char side[]={'S','W','N','E'};
-char st[]={'C','D','S','H'};
-char cd[]={'2','3','4','5','6','7','8','9','T','J','Q','K','A'};
-map<char,int> sideM,stM,cdM;
+int N,M,R,RB;
+int v[MAXN], vlow[MAXN];
+vector<int> adj[MAXN];
+set<string> ans;
+map<string,int> m;
+string ns[MAXN];
+char buf[35], buf2[35];
 
-bool comp(data a, data b)
-{
-    int x=stM[a.suit], _x=stM[b.suit];
-    int y=cdM[a.card], _y=cdM[b.card];
-
-    if(x!=_x) return x<_x;
-    else return y<_y;
+void dfs(int x, int pre, int lb){
+	vlow[x] = v[x] = lb;
+	bool art = 0;
+	REP(i,0,adj[x].size()){
+		int y = adj[x][i];
+		if(y==pre) continue;
+		if(!v[y]){
+			if(x==R) RB++;
+			dfs(y, x, lb+1);
+			vlow[x] = min(vlow[x], vlow[y]);
+			if(vlow[y]>=v[x]) art = 1;
+		}else vlow[x] = min(vlow[x], v[y]);
+	}
+	if(x==R) {if(RB>1)ans.insert(ns[x]);}
+	else {if(art)ans.insert(ns[x]);}
 }
 
-int main()
-{
-    for(int i=0; i<4; i++) sideM[side[i]]=i;
-    for(int i=0; i<4; i++) stM[st[i]]=i;
-    for(int i=0; i<13; i++) cdM[cd[i]]=i;
 
-    char start;
-    int indx;
-    data in[55];
-    vector<data> pl[4];
-
-    while(scanf(" %c",&start)==1)
-    {
-        if(start=='#') return 0;
-
-        for(int i=0; i<52; i++)
-            scanf(" %c %c",&in[i].suit,&in[i].card);
-
-        indx=sideM[start];
-        for(int i=0; i<52; i++)
-        {
-            indx++;
-            indx%=4;
-            pl[indx].push_back(in[i]);
-        }
-
-        for(int i=0; i<4; i++)
-        {
-            printf("%c:",side[i]);
-            sort(pl[i].begin(), pl[i].end(), comp);
-            for(int j=0; j<pl[i].size(); j++) printf(" %c%c",pl[i][j].suit,pl[i][j].card);
-            puts("");
-        }
-        for(int i=0; i<4; i++) pl[i].clear();
-    }
-    return 0;
+int main(){
+	int csn = 0;
+	while(1){
+		scanf("%d", &N);
+		if(!N) break;
+		REP(i,0,N){
+			scanf("%s", buf);
+			m[buf] = i;
+			ns[i] = buf;
+			adj[i].clear();
+		}
+		scanf("%d", &M);
+		REP(i,0,M){
+			scanf("%s%s", buf, buf2);
+			int a = m[buf], b = m[buf2];
+			adj[a].push_back(b);
+			adj[b].push_back(a);
+		}
+		ans.clear();
+		FILL(v,0); FILL(vlow,0);
+		REP(i,0,N) if(!v[i]) { RB=0;R=i;dfs(i,-1,1); }
+		if(csn) puts("");
+		printf("City map #%d: %d camera(s) found\n", ++csn, ans.size());
+		set<string>::iterator r;
+		for(r=ans.begin(); r!=ans.end(); r++) printf("%s\n", r->c_str());
+	}
+	return 0;
 }
