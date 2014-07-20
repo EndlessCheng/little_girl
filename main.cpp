b2b6b0936c70ac5842a5cc6bf464d2a36b3eebea
EndlessCheng <loli.con@qq.com>
2014-07-20 21:06:42 +0800

    448c
    DP

--- main.cpp
+++ main.cpp
@@ -1,25 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int a[5005];
+int h[5001], dp[5001][5001];
 
-int solve(int l, int r, int h) /*  @Tideline 超级好理解的分治算法   */
-{
-	if (l > r) return 0;
-	if (l == r) return 1;
-	int m = *min_element(a + l, a + r + 1);
-	if (m > h) return min(m - h + solve(l, r, m), r - l + 1);
-	// now m == h
-	int cnt = 0, p = l;
-	for (int i = l; i <= r; ++i) if (a[i] == m) cnt += solve(p, i - 1, h), p = i + 1;
-	cnt += solve(p, r, h);
-	return cnt;
-}
+inline int imin(int a, int b) {return ~a && a < b ? a : b;}
 
 int main()
 {
 	int n;
 	scanf("%d", &n);
-	for (int i = 1; i <= n; ++i) scanf("%d", &a[i]);
-	printf("%d", solve(1, n, 0));
+	for (int i = 1; i <= n; ++i) scanf("%d", &h[i]);
+	memset(dp, -1, sizeof(dp));
+	dp[0][0] = 0;
+	for (int i = 1; i <= n; ++i) /// 前i个fences已经涂完了
+		for (int j = 0; j < i; ++j) /// 有h[j]行是横着涂的
+			if (~dp[i - 1][j])
+			{
+				dp[i][i] = imin(dp[i][i], dp[i - 1][j] + max(0, h[i] - h[j]));
+				if (h[i] > h[j]) dp[i][j] = dp[i - 1][j] + 1; /// 说明此时需要竖着涂一次
+			}
+	int ans = -1;
+	for (int i = 0; i <= n; ++i)
+		if (~dp[n][i]) ans = imin(ans, dp[n][i]);
+	printf("%d", ans);
 }
