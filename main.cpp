c7805d6b0e514beb21d0e8e04e13e1554c501ee3
EndlessCheng <loli.con@qq.com>
2014-07-21 14:18:50 +0800

    UVa 12219

--- main.cpp
+++ main.cpp
@@ -1,39 +1,85 @@
+/* 0.996s */
 #include<bits/stdc++.h>
 using namespace std;
-const int MASK = 1 << 16;
 
-int a[100005], c[17][MASK], N;
+const int mx = 60000;
+int T, cas, cnt;
+char expr[mx * 5], *p;
+int done[mx];
+
+struct Node
+{
+	string s;
+	int ha, left, right;
+	bool operator < (const Node &b) const
+	{
+		if (ha != b.ha) return ha < b.ha;
+		   if (left != b.left) return left < b.left; // 哈希相同则比较出现的顺序
+		return right < b.right;
+	}
+} node[mx];
+
+map<Node, int> dict;
+
+int parse()
+{
+	int id = cnt++;
+	Node& u = node[id];
+	u.left = u.right = -1;
+	u.s = "";
+	u.ha = 0;
+	while (isalpha(*p))
+	{
+		u.ha = u.ha * 27 + *p - 'a' + 1;
+		u.s.push_back(*p);
+		p++;
+	}
+	if (*p == '(') /// (L,R)
+	{
+		p++;
+		u.left = parse();
+		p++;
+		u.right = parse();
+		p++;
+	}
+	if (dict.count(u) != 0)
+	{
+		id--;
+		cnt--;
+		return dict[u];
+	}
+	return dict[u] = id;
+}
+
+void print(int v)
+{
+	if (done[v] == cas) printf("%d", v + 1);
+	else
+	{
+		done[v] = cas; // 避免memset(done, 0, sizeof(done))
+		   printf("%s", node[v].s.c_str());
+		if (node[v].left != -1)
+		{
+			putchar('(');
+			print(node[v].left);
+			putchar(',');
+			print(node[v].right);
+			putchar(')');
+		}
+	}
+}
 
 int main()
 {
-	freopen("in.txt", "r", stdin);
-	for (int CAS = 1; scanf("%d", &N), ~N; CAS++)
+	scanf("%d", &T);
+	for (cas = 1; cas <= T; cas++)
 	{
-		memset(c, 0, sizeof(c));
-		for (int n = 0; n < N; n++)
-		{
-			scanf("%d", &a[n]);
-			for (int i = 0; i < 16; i++)
-				c[i][a[n] % (1 << (i + 1))]++;
-		}
-		long long ans = 0, add = 0;
-		char ch;
-		for (int b; scanf(" %c", &ch) == 1 && ch != 'E' && scanf(" %d", &b) == 1;)
-		{
-			if (ch == 'Q')
-			{
-				int more = add % (1 << (b + 1)), MAX = (1 << (b + 1));
-				for (int i = 0; i < MAX; i++)
-					if ((more + i) & (1 << b))
-						ans += c[b][i];
-			}
-			else
-			{
-				add += b;
-				add %= MASK;
-			}
-		}
-		printf("Case %d: %lld\n", CAS, ans);
+		dict.clear();
+		cnt = 0;
+		scanf("%s", expr);
+		p = expr;
+		print(parse());
+		putchar(10);
 	}
 	return 0;
 }
