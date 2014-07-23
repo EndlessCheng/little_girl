40d7111b948ec862932e8c78e6f138c511272b7c
EndlessCheng <loli.con@qq.com>
2014-07-23 15:21:55 +0800

    UVa 170

--- main.cpp
+++ main.cpp
@@ -1,91 +1,72 @@
 #include <iostream>
 #include <string>
-#include <cstring>
-#include <map>
-#include <cstdio>
 #include <vector>
-#include <queue>
+#include <stack>
+#include <map>
+#include <iomanip>
 using namespace std;
-int main()
-{
-    int n,i,k,f=1;
-    while(scanf("%d",&n)==1)
-    {
-        if(!f) printf("\n");
-        f=0;
-        vector<int>G[1000];
-        string s1, s2;
-        string s[1000];
-        int par[1000],taken[1000]={0};
-        map<string,int>mymap;
-        for(i=1,k=1;i<=n;i++)
-        {
-            cin>>s1>>s2;
-            int x,y;
-            if(!(x=mymap[s1]))
-            {
-                s[k]=s1;
-                x=mymap[s1]=k;
-                k++;
-            }
-            if(!(y=mymap[s2]))
-            {
-                s[k]=s2;
-                y=mymap[s2]=k++;
-            }
-            if(y!=x)
-            {
-                G[x].push_back(y);
-                G[y].push_back(x);
-            }
-        }
-        cin>>s1>>s2;
-        if(s1==s2)
-        {
-            cout<<s1<<" "<<s2<<endl;
-        }
 
-       else
-        {
-            int tar, src;
-            if(!(tar=mymap[s1])) tar=mymap[s1]=k++;
-            if(!(src=mymap[s2])) src=mymap[s2]=k++;
-            queue<int>Q;
-            if(src!=tar)
-            {
-                Q.push(src);
-                taken[src]=src;
-                while(!Q.empty())
-                {
-                    int u=Q.front();
-                    for(i=0;i<G[u].size();i++)
-                    {
-                        int v=G[u][i];
-                        if(v==tar)
-                        {
-                            taken[v]=u;
-                            if(v==tar) break;
-                        }
-                        if(!taken[v])
-                        {
-                            taken[v]=u;
-                            Q.push(v);
-                        }
+int main (void) {
+	vector<string> cards;
+	string input;
+	stack<string> stacks[13];
+	map<char, int> m;
 
-                    }
-                    if(i<G[u].size()) break;
-                    Q.pop();
-                }
-                if(!Q.empty())
-                while(tar!=src)
-                {
-                    cout<<s[tar]<<" "<<s[taken[tar]]<<endl;
-                    tar=taken[tar];
-                }
-                else printf("No route\n");
-            }
-        }
+	m['A'] = 0;
+	m['2'] = 1;
+	m['3'] = 2;
+	m['4'] = 3;
+	m['5'] = 4;
+	m['6'] = 5;
+	m['7'] = 6;
+	m['8'] = 7;
+	m['9'] = 8;
+	m['T'] = 9;
+	m['J'] = 10;
+	m['Q'] = 11;
+	m['K'] = 12;
 
-    }
-    return 0;
+	while (cin >> input) {
+		if (input == "#") break;
+
+		cards.clear();
+		for (int i = 0; i < 13; i++) {
+			while (!stacks[i].empty())
+				stacks[i].pop();
+		}
+
+		cards.push_back(input);
+		for (int i = 0; i < 51; i++) {
+			cin >> input;
+			cards.push_back(input);
+		}
+
+		reverse (cards.begin(), cards.end());
+
+		for (int i = 0; i < 52; i++) {
+			stacks[i % 13].push(cards[i]);
+		}
+
+		int pos = 12;
+		bool found = false;
+		string res;
+
+		int counter = 0;
+
+		while (true) {
+			if (stacks[pos].empty())
+				break;
+
+			res = stacks[pos].top();
+			stacks[pos].pop();
+
+			pos = m[res[0]];
+			counter++;
+		}
+
+		if (counter < 10) cout << 0;
+		cout << counter << "," << res << endl;
+	}
+
+	return 0;
 }
