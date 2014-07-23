5d76ce25152968925cfaf014023def830efb31d3
EndlessCheng <loli.con@qq.com>
2014-07-23 15:23:56 +0800

    UVa 356

--- main.cpp
+++ main.cpp
@@ -1,72 +1,19 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <stack>
-#include <map>
-#include <iomanip>
-using namespace std;
-
-int main (void) {
-	vector<string> cards;
-	string input;
-	stack<string> stacks[13];
-	map<char, int> m;
-
-	m['A'] = 0;
-	m['2'] = 1;
-	m['3'] = 2;
-	m['4'] = 3;
-	m['5'] = 4;
-	m['6'] = 5;
-	m['7'] = 6;
-	m['8'] = 7;
-	m['9'] = 8;
-	m['T'] = 9;
-	m['J'] = 10;
-	m['Q'] = 11;
-	m['K'] = 12;
-
-	while (cin >> input) {
-		if (input == "#") break;
-
-		cards.clear();
-		for (int i = 0; i < 13; i++) {
-			while (!stacks[i].empty())
-				stacks[i].pop();
-		}
-
-		cards.push_back(input);
-		for (int i = 0; i < 51; i++) {
-			cin >> input;
-			cards.push_back(input);
-		}
-
-		reverse (cards.begin(), cards.end());
-
-		for (int i = 0; i < 52; i++) {
-			stacks[i % 13].push(cards[i]);
-		}
-
-		int pos = 12;
-		bool found = false;
-		string res;
-
-		int counter = 0;
-
-		while (true) {
-			if (stacks[pos].empty())
-				break;
-
-			res = stacks[pos].top();
-			stacks[pos].pop();
-
-			pos = m[res[0]];
-			counter++;
-		}
-
-		if (counter < 10) cout << 0;
-		cout << counter << "," << res << endl;
-	}
-
-	return 0;
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n, i, j, flag = 0;
+    while(scanf("%d", &n)!=EOF)
+    {
+        if(flag)    puts("");
+        flag = 1;
+        int t = 0;
+        double r = n-0.5, r1, r2;//r=(2n-1)/2;
+        for(i = 1; i <= n; i++)
+            for(j = 1; j <= n; j++)
+                if(i*i + j*j < r*r) t++;
+        printf("In the case n = %d, %d cells contain segments of the circle.\n", n, n*8-4);
+        printf("There are %d cells completely contained in the circle.\n", t*4);
+    }
+    return 0;
 }
