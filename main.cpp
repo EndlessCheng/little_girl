8fc4da28aca39cbe7d620d58e02801965be4b2b9
EndlessCheng <loli.con@qq.com>
2014-07-25 16:51:26 +0800

    POJ 1609

--- main.cpp
+++ main.cpp
@@ -1,33 +1,46 @@
-#include<iostream>
-#include<iomanip>
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
 #include<algorithm>
-#include<cmath>
-#define pi (2.0*asin(1.0))
-
+#include<iostream>
+#include<string>
 using namespace std;
+
+struct node
+{
+	int x, y;
+} v[10005];
+
+bool cmp(node p, node q)
+{
+	if (p.x == q.x) return p.y < q.y;
+	return p.x < q.x;
+}
+
+int b[10005];
 
 int main()
 {
-    int i,j,n;
-    double s,r,rad[505];
-    while(cin>>n>>r && n+r)
-    {
-        s=0;
-        for(i=0;i<n;i++)
-        {
-            cin>>rad[i];
-            rad[i]*=pi/180;
-        }
-        sort(rad,rad+n);
-        for(i=0;i<n-1;i++)
-        {
-            for(j=i+1;j<n;j++)
-            {
-                s+=abs(sin(rad[j]-rad[i]));
-            }
-        }
-        s*=r*r/2;
-        cout<<fixed<<setprecision(0)<<s<<endl;
-    }
-    return 0;
+	int n, i, j, t, k;
+	while (scanf("%d", &n), n)
+	{
+		for (i = 0; i < n; i++)
+			scanf("%d%d", &v[i].x, &v[i].y);
+		sort(v, v + n, cmp);
+		t = 1;
+		b[0] = v[0].y;
+		for (i = 1; i < n; i++)
+            if (v[i].y < b[t - 1])
+			{
+				for (j = 0; j < t; j++) if (b[j] > v[i].y)
+					{
+						b[j] = v[i].y;
+						break;
+					}
+			}
+			else b[t++] = v[i].y;
+		printf("%d\n", t);
+	}
+	printf("*\n");
+	return 0;
 }
