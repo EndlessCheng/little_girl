79921e2c183496572d0decf86a265572980becac
EndlessCheng <loli.con@qq.com>
2014-07-28 20:56:46 +0800

    UVa 10622

--- main.cpp
+++ main.cpp
@@ -1,30 +1,28 @@
-#include<iostream>
-#include<vector>
-#include<stdio.h>
-#include<algorithm>
-using namespace std;
-int main ()
-{
-    int i,j,k,l,a[50010],N,Q;
-    while(scanf("%d",&N)!=EOF){
-    for(i=0;i<N;i++)scanf("%d",&a[i]);
-    vector<int>v(a,a+N);
-    vector<int>::iterator low,up;
-    scanf("%d",&Q);
-    for(i=1;i<=Q;i++)
-    {
-        scanf("%d",&l);
-        low=lower_bound (v.begin(), v.end(), l);
-        int pos_l=low-v.begin();
-        int indx_l=pos_l-1;
-        up= upper_bound (v.begin(), v.end(), l);
-        int pos_u=up-v.begin();
-        int indx_u=pos_u-1;
-        if(indx_l<0)printf("X ");
-        else{printf("%d ",a[indx_l]);}
-        if(indx_u+1>N-1)printf("X\n");
-        else{printf("%d\n",a[indx_u+1]);}
-    }
-    }
-    return 0;
+#include <stdio.h>
+#define LL long long
+
+int main() {
+	LL i, n;
+	while(scanf("%lld", &n) != -1 && n) {
+		int flag = 1;
+		if(n < 0)
+			n = -n, flag = 0;
+		int ans = 1;
+		int ok = 0;
+		for(i = 2;i*i <= n; i++) {
+			LL cur = 1;
+			int sum = 0;
+			while(cur <= n) {
+				cur *= i;
+				sum++;
+				if(cur == n && (flag || sum&1)) {
+					ok = 1;
+					ans = sum;
+				}
+			}
+			if(ok)	break;
+		}
+		printf("%d\n", ans);
+	}
+	return 0;
 }
