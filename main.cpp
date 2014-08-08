d7683414ca7a967a428de613a14946af9629f5d2
EndlessCheng <loli.con@qq.com>
2014-08-08 10:49:18 +0800

    CF 343D

--- main.cpp
+++ main.cpp
@@ -1,24 +1,49 @@
-/* 124 ms, 2324 KB */
-#include<cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+const int mx = 5e5 + 5;
 
-int a[300005], w[300005];
+vector<int> g[mx];
+set<int> Empty;
+int L[mx], R[mx], fa[mx];
+int timer, to;
+
+void dfs(int v, int p = -1)
+{
+    L[v] = ++timer;
+    for (int i = 0; i < g[v].size(); ++i)
+        if ((to = g[v][i]) != p)
+            fa[to] = v, dfs(to, v);
+    R[v] = timer;
+    if (R[v] == L[v]) Empty.insert(L[v]);
+}
+
+bool empty(int v)
+{
+    set<int>::iterator it = Empty.lower_bound(L[v]);
+    return it != Empty.end() && *it <= R[v];
+}
 
 int main()
 {
-	int n, i, l, r, cnt = 0, maxd = 0;
-	scanf("%d", &n);
-	for (i = 0; i < n; i++) scanf("%d", &a[i]);
-	for (i = 0; i < n;)
-	{
-		l = r = i;
-		while (l && a[l - 1] % a[i] == 0) l--;
-		while (r < n - 1 && a[r + 1] % a[i] == 0) r++;
-		i = r + 1; // 关键一步，保证算法是O(n)的，这样做是因为在[l,r]内的数的「边界」就是[l,r]（可用反证法思考）
-		r -= l;
-		if (r > maxd) cnt = 0, maxd = r;
-		if (r == maxd) w[cnt++] = l + 1;
-	}
-	printf("%d %d\n", cnt, maxd);
-	for (i = 0; i < cnt; ++i) printf("%d ", w[i]);
-	return 0;
+    int n, q, a, b, op, v;
+    scanf("%d", &n);
+    while (--n)
+    {
+        scanf("%d%d", &a, &b);
+        g[a].push_back(b), g[b].push_back(a);
+    }
+    dfs(1);
+    scanf("%d", &q);
+    while (q--)
+    {
+        scanf("%d%d", &op, &v);
+        if (op == 1)
+        {
+            if (fa[v] && empty(fa[v])) Empty.insert(L[fa[v]]);
+            Empty.erase(Empty.lower_bound(L[v]), Empty.upper_bound(R[v]));
+        }
+        else if (op == 2) Empty.insert(L[v]);
+        else puts(empty(v) ? "0" : "1");
+    }
+    return 0;
 }
