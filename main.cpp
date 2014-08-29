ff560b301be7f3632e209438b79a4e1d9050e6b4
EndlessCheng <loli.con@qq.com>
2014-08-29 22:54:06 +0800

    Python 二分浮点

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+import math
 
+x = float(raw_input('Input: '))
+
+low = 0.0
+high = x
+ans = (low + high) / 2
+eps = 1e-9
+
+while low + eps < high and ans * ans != x:
+	if ans * ans < x:
+		low = ans
+	else:
+		high = ans
+	ans = (low + high) / 2
+
+if math.fabs(ans) > eps:
+	print -ans, ans
+else:
+	print ans
