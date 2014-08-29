4e411ce71467cfe6154ddd045a461a65518f02cd
EndlessCheng <loli.con@qq.com>
2014-08-29 23:31:15 +0800

    猜单词

--- main.cpp
+++ main.cpp
@@ -1,20 +1,35 @@
-import math
+import random
 
-x = float(raw_input('Input: '))
+filename = 'words.dic'
+f = open(filename, 'r')
+allLines = f.readlines()
+f.close()
 
-low = 0.0
-high = x
-ans = (low + high) / 2
-eps = 1e-9
+i = random.randint(0, len(allLines) - 1)
+words = allLines[i].split()
+i = random.randint(0, len(words) - 1)
+word = words[i]
 
-while low + eps < high and ans * ans != x:
-	if ans * ans < x:
-		low = ans
+sz = len(word)
+guess = '*' * sz
+cnt = 0
+
+while(guess.count('*') != 0):
+	ch = raw_input('Enter a letter in word ' + guess + ':')
+	if word.find(ch) == -1:
+		print ch + ' is not in the word'
+		cnt += 1
+	elif guess.find(ch) != -1:
+		print ch + ' is already in the word'
 	else:
-		high = ans
-	ans = (low + high) / 2
-
-if math.fabs(ans) > eps:
-	print -ans, ans
-else:
-	print ans
+		tmp = ''
+		for i in range(sz):
+			if guess[i] != '*':
+				tmp += guess[i]
+			elif word[i] == ch:
+				tmp += ch
+			else:
+				tmp += '*'
+		guess = tmp
+	
+print 'The word is %s. You missed %d time(s).' % (word, cnt)
