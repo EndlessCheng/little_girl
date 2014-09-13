3fb9adf709933d95209516abfd3768fc88aff1f9
EndlessCheng <loli.con@qq.com>
2014-09-13 21:34:01 +0800

    鞍山网赛C题

--- main.cpp
+++ main.cpp
@@ -1,93 +1,116 @@
-
+#include <vector>
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-typedef long long Long;
+int n;
 
-const int N = 10;
-const int M = N * (N - 1) / 2;
-const int MOD = (int)1e9 + 7;
+const int N = 40;
 
-Long two[M + 1];
-bool graph[N][N];
+char sym[N];
+unsigned int num[N];
 
-unsigned char cut[1 << N][1 << N];
-Long connected[1 << N], biconnected[1 << N], go[1 << N][1 << N];
-
-int main()
-{
-    two[0] = 1;
-    for (int i = 1; i <= M; ++ i) {
-        two[i] = two[i - 1] << 1;
+inline unsigned int trans(char ch) {
+    if (ch >= '0' && ch <= '9') {
+        return ch - '0';
+    } else {
+        return ch - 'a' + 10;
     }
-    int test_count;
-    scanf("%d", &test_count);
-    while (test_count --) {
-        int n, m;
-        scanf("%d%d", &n, &m);
-        memset(graph, true, sizeof(graph));
-        for (int i = 0; i < n; ++ i) {
-            graph[i][i] = false;
+}
+
+unsigned int getNum(char *s) {
+    unsigned int ret = 0;
+    for (int i = 0; i < 8; ++i) {
+        ret = ret << 4 | trans(s[i]);
+    }
+    return ret;
+}
+
+int stamp, vis[1 << 20], qhead, qtail, qtail2;
+unsigned int best[2][1 << 20], q[2][1 << 20];
+
+int b[N];
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &n);
+        {
+            int tot = 0;
+            char op[3], buf[10];
+            for (int i = 0; i < n; ++i) {
+                scanf("%s%s", op, buf);
+                unsigned int x = getNum(buf);
+                if (op[0] == '-') {
+                    op[0] = '+';
+                    x = (~x) + 1;
+                }
+                if (tot && sym[tot - 1] == '+' && op[0] == '+') {
+                    num[tot - 1] += x;
+                } else {
+                    sym[tot] = op[0];
+                    num[tot] = x;
+                    ++tot;
+                }
+            }
+            n = tot;
         }
-        for (int i = 0; i < m; ++ i) {
-            int a, b;
-            scanf("%d%d", &a, &b);
-            a --;
-            b --;
-            graph[a][b] = graph[b][a] = false;
+        {
+            qhead = 0, qtail = 1;
+            q[0][qhead] = 0;
+            best[0][0] = 0;
         }
-        for (int a = 0; a < 1 << n; ++ a) {
-            for (int b = 0; b < 1 << n; ++ b) {
-                if (a == b || !(a & b)) {
-                    unsigned char &ref = cut[a][b];
-                    ref = 0;
-                    for (int i = 0; i < n; ++ i) {
-                        if (a >> i & 1) {
-                            for (int j = a == b ? i + 1 : 0; j < n; ++ j) {
-                                ref += (b >> j & 1) && graph[i][j];
+        int now = 0;
+        for (int bit = 0; bit < 32; ++bit) {
+            for (int i = 0; i < n; ++i) {
+                b[i] = num[i] >> bit & 1;
+            }
+            int next = now ^ 1;
+            qtail2 = 0;
+            ++stamp;
+            while (qhead < qtail) {
+                int st = q[now][qhead++];
+                for (int c = 0; c < 2; ++c) {
+                    unsigned cur = c;
+                    int nst = 0, cnt = 0;
+                    for (int i = 0; i < n; ++i) {
+                        if (sym[i] == '+') {
+                            cur += b[i] + (st >> cnt & 1);
+                            if (cur & 2) {
+                                nst |= 1 << cnt;
+                                cur ^= 2;
+                            }
+                            ++cnt;
+                        } else {
+                            if (sym[i] == '&') {
+                                cur &= b[i];
+                            } else if (sym[i] == '|') {
+                                cur |= b[i];
+                            } else {
+                                cur ^= b[i];
                             }
                         }
                     }
-                }
-            }
-        }
-        connected[0] = biconnected[0] = 1;
-        for (int mask = 1; mask < 1 << n; ++ mask) {
-            int lowbit = mask & -mask;
-            {
-                Long &ref = connected[mask];
-                ref = two[cut[mask][mask]];
-                for (int subset = mask ^ lowbit; subset > 0; subset = subset - 1 & (mask ^ lowbit)) {
-                    ref -= connected[mask ^ subset] * two[cut[subset][subset]];
-                }
-            }
-            {
-                for (int target = mask ^ lowbit; target > 0; target = target - 1 & (mask ^ lowbit)) {
-                    int source = mask ^ target;
-                    Long &ref = go[mask][source];
-                    ref = 0;
-                    int lowbit = target & -target;
-                    for (int subset = target ^ lowbit; ; subset = subset - 1 & (target ^ lowbit)) {
-                        int set = target ^ subset;
-                        ref += cut[source][set] * connected[set] * go[mask ^ set][source];
-                        if (subset == 0) {
-                            break;
-                        }
+                    if (vis[nst] != stamp) {
+                        vis[nst] = stamp;
+                        best[next][nst] = 0;
+                        q[next][qtail2] = nst;
+                        ++qtail2;
                     }
-                }
-                go[mask][mask] = 1;
-            }
-            {
-                Long &ref = biconnected[mask];
-                ref = connected[mask];
-                for (int subset = mask ^ lowbit; subset > 0; subset = subset - 1 & (mask ^ lowbit)) {
-                    ref -= biconnected[mask ^ subset] * go[mask][mask ^ subset];
+                    best[next][nst] = max(best[next][nst], (best[now][st] | (cur << bit)));
                 }
             }
+            now = next;
+            qhead = 0, qtail = qtail2;
         }
-        int result = biconnected[(1 << n) - 1] % MOD;
-        std::cout << result << std::endl;
+        unsigned int ans = 0;
+        for (int qhead = 0; qhead < qtail; ++qhead) {
+            ans = max(ans, best[now][q[now][qhead]]);
+        }
+        printf("%u\n", ans);
     }
     return 0;
 }
