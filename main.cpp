505d230a059a0e47ce5ba0f5e86c117d335739c1
EndlessCheng <loli.con@qq.com>
2014-09-13 21:42:41 +0800

    鞍山网赛H题KAMI

--- main.cpp
+++ main.cpp
@@ -1,116 +1,157 @@
+#include <iostream>
 #include <vector>
+#include <queue>
 #include <cstdio>
 #include <cstring>
-#include <iostream>
-#include <algorithm>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
-int n;
+const int n = 16, m = 10;
+int mp[10][18][12], id[18][12], mp2[170][170], co[15], vis[170], px[170], py[170];
+int t, ln, mn;
+int xx[5] = {0, 1, 0, -1}, yy[5] = {1, 0, -1, 0};
+char c;
+vector <int> g;
 
-const int N = 40;
-
-char sym[N];
-unsigned int num[N];
-
-inline unsigned int trans(char ch) {
-    if (ch >= '0' && ch <= '9') {
-        return ch - '0';
-    } else {
-        return ch - 'a' + 10;
+void dfs (int c, int x ,int y) {
+    id[x][y] = ln;
+    for (int i = 0; i < 4; i++) {
+        int dx = x + xx[i], dy = y + yy[i];
+        if (dx > n || dx < 1 || dy > m || dy < 1 || id[dx][dy]) continue;
+        if (mp[0][dx][dy] != c) continue;
+        dfs (c, dx, dy);
     }
 }
 
-unsigned int getNum(char *s) {
-    unsigned int ret = 0;
-    for (int i = 0; i < 8; ++i) {
-        ret = ret << 4 | trans(s[i]);
+int d[170];
+
+void bfs (int k) {
+    queue <int> q;
+    q.push(k);
+    memset (d, 0, sizeof d);
+    d[k] = 1;
+    int m = 0;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (int i = 1; i <= ln; i++) {
+            if (!d[i] && mp2[u][i]) {
+                d[i] = d[u] + 1;
+                q.push(i);
+                m = max (d[i], m);
+            }
+        }
     }
-    return ret;
+    if (m == mn) {
+        g.push_back(k);
+    } else if (m < mn) {
+        mn = m;
+        g.clear();
+        g.push_back(k);
+    }
 }
 
-int stamp, vis[1 << 20], qhead, qtail, qtail2;
-unsigned int best[2][1 << 20], q[2][1 << 20];
-
-int b[N];
-
-int main() {
-    int t;
-    scanf("%d", &t);
-    while (t--) {
-        scanf("%d", &n);
-        {
-            int tot = 0;
-            char op[3], buf[10];
-            for (int i = 0; i < n; ++i) {
-                scanf("%s%s", op, buf);
-                unsigned int x = getNum(buf);
-                if (op[0] == '-') {
-                    op[0] = '+';
-                    x = (~x) + 1;
-                }
-                if (tot && sym[tot - 1] == '+' && op[0] == '+') {
-                    num[tot - 1] += x;
-                } else {
-                    sym[tot] = op[0];
-                    num[tot] = x;
-                    ++tot;
-                }
+int jug (int k, int l) {
+    int vis2[5], sum = 0;
+    memset (vis2, 0, sizeof vis2);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            int tmp = mp[k][i][j];
+//            cout << tmp ;
+            if (!vis2[tmp]) {
+                vis2[tmp] = 1;
+                sum++;
             }
-            n = tot;
         }
-        {
-            qhead = 0, qtail = 1;
-            q[0][qhead] = 0;
-            best[0][0] = 0;
-        }
-        int now = 0;
-        for (int bit = 0; bit < 32; ++bit) {
-            for (int i = 0; i < n; ++i) {
-                b[i] = num[i] >> bit & 1;
-            }
-            int next = now ^ 1;
-            qtail2 = 0;
-            ++stamp;
-            while (qhead < qtail) {
-                int st = q[now][qhead++];
-                for (int c = 0; c < 2; ++c) {
-                    unsigned cur = c;
-                    int nst = 0, cnt = 0;
-                    for (int i = 0; i < n; ++i) {
-                        if (sym[i] == '+') {
-                            cur += b[i] + (st >> cnt & 1);
-                            if (cur & 2) {
-                                nst |= 1 << cnt;
-                                cur ^= 2;
-                            }
-                            ++cnt;
-                        } else {
-                            if (sym[i] == '&') {
-                                cur &= b[i];
-                            } else if (sym[i] == '|') {
-                                cur |= b[i];
-                            } else {
-                                cur ^= b[i];
-                            }
-                        }
-                    }
-                    if (vis[nst] != stamp) {
-                        vis[nst] = stamp;
-                        best[next][nst] = 0;
-                        q[next][qtail2] = nst;
-                        ++qtail2;
-                    }
-                    best[next][nst] = max(best[next][nst], (best[now][st] | (cur << bit)));
-                }
-            }
-            now = next;
-            qhead = 0, qtail = qtail2;
-        }
-        unsigned int ans = 0;
-        for (int qhead = 0; qhead < qtail; ++qhead) {
-            ans = max(ans, best[now][q[now][qhead]]);
-        }
-        printf("%u\n", ans);
+//        cout << endl;
     }
+    if (sum > l) return 1;
+    return 0;
+}
+
+int c1, c2;
+
+void cg (int k, int i, int j) {
+    mp[k][i][j] = c2;
+    int dx, dy;
+    for (int d = 0; d < 4; d++) {
+        dx = i + xx[d], dy = j + yy[d];
+        if (mp[k][dx][dy] == c1) cg (k, dx, dy);
+    }
+}
+
+int dfs2 (int k, int x, int y) {
+    if (t - k < 4 && jug (k, t - k + 1)) {
+//        cout << 'x' <<endl;
+        return 0;
+    }
+    if (k == t) {
+        return 1;
+    }
+    for (int i = 1; i <= 4; i++) {
+        if (i == mp[k][x][y]) continue;
+        co[k] = i;
+        memcpy (mp[k + 1], mp[k], sizeof mp[k]);
+        c1 = mp[k][x][y], c2 = i;
+//        cout << k << ' ' << c1 << ' ' << c2 << endl;
+        cg(k + 1, x, y);
+        if (dfs2 (k + 1, x, y)) return 1;
+    }
+    return 0;
+}
+
+int main()
+{
+//    freopen ("in", "r", stdin);
+    srand (time (NULL));
+    int T, cas = 1;
+    cin >> T;
+    while (T--) {
+        cin >> t;
+        g.clear();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                cin >> c;
+                mp[0][i][j] = c - '0';
+            }
+        }
+        memset (mp2, 0, sizeof mp2);
+        memset (id, 0, sizeof id);
+        ln = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) if (!id[i][j]){
+                ln++;
+                px[ln] = i, py[ln] = j;
+                dfs (mp[0][i][j], i, j);
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                for (int k = 0; k < 4; k++) {
+                    int dx = i + xx[k], dy = j + yy[k];
+                    if (dx > n || dx < 1 || dy > m || dy < 1) continue;
+                    int id1 = id[i][j], id2 = id[dx][dy];
+                    if (id1 != id2) mp2[id1][id2] = mp2[id2][id1] = 1;
+                }
+            }
+        }
+        mn = 1e9;
+        for (int i = 1; i <= ln; i++) {
+            bfs (i);
+        }
+//        int sz = g.size();
+        int x, y;
+        printf ("Case #%d:\n", cas++);
+        for (int tmp = 1; tmp <= ln; tmp++)
+//            int j = px[tmp], k = py[tmp];
+            if (dfs2 (0, px[tmp], py[tmp])) {
+                x = px[tmp], y = py[tmp];
+                for (int k = 0; k < t; k++) {
+                    cout << co[k] << ' ' << x << ' ' << y << endl;
+                }
+                break;
+            }
+        }
     return 0;
 }
